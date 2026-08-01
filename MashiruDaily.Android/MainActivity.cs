e05208e45d5a1afd7233142867c8f6ec55d7f5ec
wan-an-zz <1433430686@qq.com>
2026-08-02 00:31:15 +0800

    chore: 新建项目文件

---
+++ MashiruDaily.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace MashiruDaily.Android;
+
+[Activity(
+    Label = "MashiruDaily.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
